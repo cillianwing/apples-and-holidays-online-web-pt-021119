b3d772927b167e2330df9dd89817437ac1432fc4
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-23 17:55:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
   holiday_hash.each do |seasons, holidays|
     if seasons == season
-      seasons[holiday_name] = supply_array
+      seasons[:holiday_name] = supply_array
     end
   end
   holiday_hash
