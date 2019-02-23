faf9cd640160e7a92d152eaa5d9428efa99e5956
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-23 17:50:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,14 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  return holiday_hash[:summer][:fourth_of_july][1]
+  #return holiday_hash[:summer][:fourth_of_july][1]
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      if holiday == :fourth_of_july 
+        return holiday[1]
+      end 
+    end 
+  end
   
 end
 
